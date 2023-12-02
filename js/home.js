9eb1cc452a300f33f031b0c3902502497fe522f4
Alexandre Silva <dev.alexandresilva@gmail.com>
2023-12-02 23:09:19 +0000

    Fix formatting and add task completion icons

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 const hoje = new Date();
-const dia = hoje.getDate();
-const mes = hoje.getMonth() + 1;
+const dia = hoje.getDate().toString().padStart(2, '0');
+const mes = (hoje.getMonth() + 1).toString().padStart(2, '0');
 const ano = hoje.getFullYear();
 const data = `${dia}/${mes}/${ano}`;
 const dataElement = document.getElementById('hoje');
