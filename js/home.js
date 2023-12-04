d0695c4934eba77f3214a0c75d33a073a6d361c5
Alexandre Silva <dev.alexandresilva@gmail.com>
2023-12-04 16:38:23 +0000

    Fix bug in concluirTarefa function

--- js/home.js
+++ js/home.js
@@ -1,19 +1,19 @@
-const hoje = new Date();
-const dia = hoje.getDate().toString().padStart(2, '0');
-const mes = (hoje.getMonth() + 1).toString().padStart(2, '0');
-const ano = hoje.getFullYear();
-const data = `${dia}/${mes}/${ano}`;
-const dataElement = document.getElementById('hoje');
-dataElement.append(data);
-
 function concluirTarefa() {
     const concluir = document.querySelectorAll('.concluir');
-    concluir.forEach((item) => {
-        const tarefaElement = item.closest('.tarefa');
-        item.addEventListener('click', () => {
-            tarefaElement.classList.toggle('tarefa-concluida');
+    
+    if (concluir.length > 0) {
+        concluir.forEach((item) => {
+            const tarefaElement = item.closest('.tarefa');
+            if (tarefaElement) {
+                item.addEventListener('click', () => {
+                    console.log('clicou');
+                    tarefaElement.classList.toggle('tarefa-concluida');
+                });
+            }
         });
-    });
+    }
 }
 
-concluirTarefa();
+document.addEventListener('DOMContentLoaded', (event) => {
+    concluirTarefa();
+});
