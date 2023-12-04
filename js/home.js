add8a0a8e785a963fcf26415df04a30de6442970
Alexandre Silva <dev.alexandresilva@gmail.com>
2023-12-04 19:42:38 +0000

    Add back button to pomodoro page

--- js/home.js
+++ js/home.js
@@ -16,4 +16,31 @@ function concluirTarefa() {
 
 document.addEventListener('DOMContentLoaded', (event) => {
     concluirTarefa();
+    document.getElementById('save-task').addEventListener('click', function() {
+        var taskName = document.getElementById('task-name').value;
+        var taskDescription = document.getElementById('task-description').value;
+    
+        // Aqui você pode adicionar o código para salvar a tarefa
+    
+        // Limpa o formulário
+        document.getElementById('task-form').reset();
+    
+        // Fecha o modal
+        var taskModal = bootstrap.Modal.getInstance(document.getElementById('taskModal'));
+        taskModal.hide();
+    });
+    document.getElementById('save-list').addEventListener('click', function() {
+        var listName = document.getElementById('list-name').value;
+        var listEmoji = document.getElementById('list-emoji').value;
+    
+        // Aqui você pode adicionar o código para salvar a lista
+    
+        // Limpa o formulário
+        document.getElementById('list-form').reset();
+    
+        // Fecha o modal
+        var listModal = bootstrap.Modal.getInstance(document.getElementById('listModal'));
+        listModal.hide();
+    });
 });
+
